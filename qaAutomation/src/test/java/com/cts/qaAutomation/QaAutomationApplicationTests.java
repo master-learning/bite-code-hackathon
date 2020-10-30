3b3f2f3f597104f88f9e7dd008784ba3d64b9a4b
Ronit Biswas <ronitbiswas@live.com>
2020-10-30 18:36:58 +0530

    Created the repo and inited codes

---
+++ qaAutomation/src/test/java/com/cts/qaAutomation/QaAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.qaAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
